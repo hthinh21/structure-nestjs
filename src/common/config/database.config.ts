064108f737b934d87c1b257272209f57543b0a84
hthinh21 <dothinh795@gmail.com>
2026-06-23 17:30:10 +0700

    check eslint

--- src/common/config/database.config.ts
+++ src/common/config/database.config.ts
@@ -38,7 +38,5 @@ export default registerAs('database', (): TypeOrmModuleOptions => {
       idleTimeoutMillis: 30_000, // Close idle connections after 30s
       connectionTimeoutMillis: 2_000, // Fail fast if DB unreachable after 2s
     },
-
-    
   };
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) =>
-                configService.get('database')!,
-        }),
-    ],
-    exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get('database')!,
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
